c8114a8f30f024d10e37f584e055d4e38011776e
Trung <trungsanglong@gmail.com>
2022-08-24 01:11:27 +0700

    Bug o cho data confirm nha a Lieu

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     validates :email, presence: true, length: {maximum: 100,minimum: 5}, format: {with: VALID_EMAIL_REGEX},
                                                                         uniqueness: true
     has_secure_password
-    validates :password, presence: true, length: {minimum: 6, maximum: 20}
+    validates :password, presence: true, length: {minimum: 6, maximum: 20}, allow_nil: true
 
     # Returns the hash digest of the given string.
     def User.digest(string)
