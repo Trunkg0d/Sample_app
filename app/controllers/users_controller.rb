25cd1fafded8ac79643355d9481cf3194040c161
Trung <trungsanglong@gmail.com>
2022-08-24 14:30:51 +0700

    Finish activating the account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # Not the final implementation!
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App"
-      redirect_to @user
+      # @user.activation_token = User.new_token
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      # log_in @user
+      # flash[:success] = "Welcome to the Sample App"
+      redirect_to root_url
       #equal to redirect_to user_url(@user)
     else
       render 'new'
