1cd6991985a7aff438fffe134bfa5e539f073122
Trunkg0d <trungsanglong@gmail.com>
2022-08-26 16:02:08 +0700

    Finish manipulating micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :microposts
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save {self.email = self.email.downcase}
     before_create :create_activation_digest
@@ -65,6 +65,13 @@ class User < ApplicationRecord
     def password_reset_expired?
         reset_sent_at < 2.hours.ago
     end        
+
+    # Defines a proto-feed.
+    # See "Following users" for the full implementation.
+    def feed
+        Micropost.where("user_id = ?", id)
+    end
+    
 
     private
         def create_activation_digest
