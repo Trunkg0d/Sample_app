a8cbdb9ebd9d5639bde4cfe5e214148fece06cd2
Trunkg0d <trungsanglong@gmail.com>
2022-08-29 23:35:50 +0700

    Fix conflict

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,14 +5,11 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 400}
-<<<<<<< HEAD
   mount_uploader :picture, PictureUploader
   # Returns a resized image for display.
   def display_image
     self.image.variant(resize_to_limit: [509, 513])
   end
-
-=======
   # validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
   #   message: "must be a valid image format" },
   #   size: { less_than: 5.megabytes,
@@ -22,6 +19,4 @@ class Micropost < ApplicationRecord
   # def display_image
   #   image.variant(resize_to_limit: [509, 513])
   # end
-  
->>>>>>> 8cad5dcef0cc9454465b3dcb7b258b5da1547dd4
 end
