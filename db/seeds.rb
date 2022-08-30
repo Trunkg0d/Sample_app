e9d7e9529e0efa5d9c852b2a72076806db99d770
Trunkg0d <trungsanglong@gmail.com>
2022-08-30 23:32:32 +0700

    Finish follow.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,25 @@ User.create!(name: "Example User",
     admin: true,
     activated: true,
     activated_at: Time.zone.now)
-    # Generate a bunch of additional users.
 
-    150.times do |n|
-    name = "Trunk thứ #{n}"
-    email = "example-#{n+1}@railstutorial.org"
-    password = "password"
-    User.create!(name: name,
-    email: email,
-    password: password,
-    password_confirmation: password,
-    activated: true,
-    activated_at: Time.zone.now)
-    end
+# Generate a bunch of additional users.
+150.times do |n|
+name = "Trunk thứ #{n}"
+email = "example-#{n+1}@railstutorial.org"
+password = "password"
+User.create!(name: name,
+email: email,
+password: password,
+password_confirmation: password,
+activated: true,
+activated_at: Time.zone.now)
+end
+
+# Create following relationships.
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
     
