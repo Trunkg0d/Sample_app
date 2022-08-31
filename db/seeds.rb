8961b7081b1ad8446a2ff1fc5dc21427284b1543
Trunkg0d <trungsanglong@gmail.com>
2022-08-31 20:19:16 +0700

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ User.create!(name: "Example User",
     activated_at: Time.zone.now)
     # Generate a bunch of additional users.
 
-    150.times do |n|
+150.times do |n|
     name = "Trunk thứ #{n}"
     email = "example-#{n+1}@railstutorial.org"
     password = "password"
@@ -17,5 +17,12 @@ User.create!(name: "Example User",
     password_confirmation: password,
     activated: true,
     activated_at: Time.zone.now)
-    end
+end
     
+# Create following relationships.
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
